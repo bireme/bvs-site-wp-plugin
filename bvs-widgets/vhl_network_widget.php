f0c3326bf4f1741b648769ff825e20d6e9e93d7f
vinicius.andrade <vinicius.andrade@bireme.org>
2011-06-08 15:34:05 -0300

    [new]  basic version of vhl network list widget

--- bvs-widgets/vhl_network_widget.php
+++ bvs-widgets/vhl_network_widget.php
@@ -1,6 +1,10 @@
 <?php
+include_once (ABSPATH . WPINC . '/feed.php');
+
 /*** VHL Network Widget ****************/
 class VHL_Network_Widget extends WP_Widget {
+
+    var $service_url = 'http://srv.bvsalud.org/bvsnet/rss';
 
     function VHL_Network_Widget() {
         $widget_ops = array('classname' => __('VHL Network'), 'description' => __('Adds a VHL network on your site') );
@@ -12,8 +16,16 @@ class VHL_Network_Widget extends WP_Widget {
         echo $before_widget;
             $blank = ($instance['target'] == 'sim') ? '_blank' : '';
             if($instance['title']) echo $before_title, $instance['title'], $after_title;
+
+            $rss_url = $this->service_url . '?' . $instance['params'];
             
+            $rss = fetch_feed($rss_url);
             
+            if (!is_wp_error( $rss ) ) { // Checks that the object is created correctly 
+                $item = $rss->get_item(0);
+                echo $item->get_description();
+            }
+
         echo $after_widget;
     }
  
@@ -21,11 +33,13 @@ class VHL_Network_Widget extends WP_Widget {
         $instance = $old_instance;
         
         $instance['title'] = strip_tags($new_instance['title']);
+        $instance['params'] = strip_tags($new_instance['params']);
         return $instance;
     }
     
-    function form($instance) {        
+    function form($instance) {
         $title = esc_attr($instance['title']);
+        $params= esc_attr($instance['params']);
         ?>
             <p>
                 <label for="<?php echo $this->get_field_id('title'); ?>">
@@ -33,6 +47,13 @@ class VHL_Network_Widget extends WP_Widget {
                     <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
                 </label>
             </p>
+            <p>
+                <label for="<?php echo $this->get_field_id('params'); ?>">
+                    <?php _e('Parâmetros adicionais:'); ?> 
+                    <input class="widefat" id="<?php echo $this->get_field_id('params'); ?>" name="<?php echo $this->get_field_name('params'); ?>" type="text" value="<?php echo $params; ?>" />
+                </label>
+            </p>
+
         <?php 
     }
 }
