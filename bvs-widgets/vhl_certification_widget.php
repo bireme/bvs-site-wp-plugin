2a2c6dde74b51a95b6a760adaf1dd0da4bccfaa8
vinicius.andrade <vinicius.andrade@bireme.org>
2011-06-14 15:44:41 -0300

    [add] widget for certification VHL seal

---
+++ bvs-widgets/vhl_certification_widget.php
@@ -0,0 +1,58 @@
+<?php
+
+/*** VHL Certification Widget ****************/
+class VHL_Certification_Widget extends WP_Widget {
+
+    var $service_url = 'http://cert.bvsalud.org/';
+
+    function VHL_Certification_Widget() {
+        $widget_ops = array('classname' => 'vhl-certification', 'description' => __('Adds a VHL certification on your site') );
+        parent::WP_Widget('vhl_certification', __('VHL Certification'), $widget_ops);
+    }
+ 
+    function widget($args, $instance) {
+        $current_language = strtolower(get_bloginfo('language'));
+        $bvs_url = substr(get_bloginfo('home'),7);      //remove http:// from url
+
+        $lng = substr($current_language, 0,2); //use first 2 letters of language code (pt-BR = pt)
+
+        extract($args);
+        echo $before_widget;
+            $blank = ($instance['target'] == 'sim') ? '_blank' : '';
+            if($instance['title']) echo $before_title, $instance['title'], $after_title;
+
+            echo '<script type="text/javascript" src="' . $this->service_url . 'code.php?bvs_url=' .$bvs_url . '&lang=' . $lng . '">//</script>';
+
+        echo $after_widget;
+    }
+ 
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        
+        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['params'] = strip_tags($new_instance['params']);
+        return $instance;
+    }
+    
+    function form($instance) {
+        $title = esc_attr($instance['title']);
+        $params= esc_attr($instance['params']);
+        ?>
+            <p>
+                <label for="<?php echo $this->get_field_id('title'); ?>">
+                    <?php _e('Title:'); ?> 
+                    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+                </label>
+            </p>
+            <!--
+            <p>
+                <label for="<?php echo $this->get_field_id('params'); ?>">
+                    <?php _e('Parâmetros adicionais:'); ?> 
+                    <input class="widefat" id="<?php echo $this->get_field_id('params'); ?>" name="<?php echo $this->get_field_name('params'); ?>" type="text" value="<?php echo $params; ?>" />
+                </label>
+            </p>
+            -->
+        <?php 
+    }
+}
+?>
