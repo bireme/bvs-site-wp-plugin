8fb8dbd7490f62c43527fea9674c77c6029c8be2
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2012-05-10 13:56:20 -0300

    Use appropriated wp functions for determine plugins path

--- bvs-core/widgets.php
+++ bvs-core/widgets.php
@@ -1,9 +1,9 @@
 <?php
 
-include_once(BVS_PATH . '/bvs-widgets/vhl_collection_widget.php');
-include_once(BVS_PATH . '/bvs-widgets/vhl_network_widget.php');
-include_once(BVS_PATH . '/bvs-widgets/vhl_search_widget.php');
-include_once(BVS_PATH . '/bvs-widgets/vhl_certification_widget.php');
+include_once(BVS_PLUGIN_PATH . '/bvs-widgets/vhl_collection_widget.php');
+include_once(BVS_PLUGIN_PATH . '/bvs-widgets/vhl_network_widget.php');
+include_once(BVS_PLUGIN_PATH . '/bvs-widgets/vhl_search_widget.php');
+include_once(BVS_PLUGIN_PATH . '/bvs-widgets/vhl_certification_widget.php');
 
 function register_vhl_widgets() {
     register_widget("VHL_Collection_Widget");
