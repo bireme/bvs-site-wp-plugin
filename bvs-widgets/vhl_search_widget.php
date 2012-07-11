0560fbe1eb2bad937d2b7a3abc3e1ae6388d7821
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2012-07-11 11:24:57 -0300

    Adjust search widget for accessibility

--- bvs-widgets/vhl_search_widget.php
+++ bvs-widgets/vhl_search_widget.php
@@ -22,7 +22,8 @@ class VHL_Search_Widget extends WP_Widget {
              echo '   <input type="hidden" name="lang" value="' . $lng . '">';
              echo '   <input type="hidden" name="home_url" value="' . get_bloginfo('home') . '">';
              echo '   <input type="hidden" name="home_text" value="' . get_bloginfo('name') . '">';
-             echo '   <input type="text" id="vhl-search-input" class="vhl-search-input" name="q" onfocus="if(this.value==\'Busca\')this.value = \'\';" onblur="if(this.value==\'\') this.value = \'Busca\';" value="Busca">';
+             echo '   <label for="vhl-search-input"></label>';
+             echo '   <input type="text" id="vhl-search-input" class="vhl-search-input" name="q" onfocus="if(this.value==\'' .__('Search') . '\') this.value = \'\';" onblur="if(this.value==\'\') this.value = \'' .__('Search') .'\';" placeholder=\''.__('Search') . '\' value="' .__('Search') . '">';
              echo '   <input type="submit" class="vhl-search-submit submit" name="submit" value="' .__('Search') .'">';
              echo '</form>';
 
