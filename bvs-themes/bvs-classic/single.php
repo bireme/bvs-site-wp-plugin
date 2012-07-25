3db3ff16153c9f80e6970c86830f607a5ef79b9c
Ricardo Santos <ricardo.santos@bireme.org>
2012-07-25 18:00:17 -0300

    Adjust styles and improve presentation of second level pages

--- bvs-themes/bvs-classic/single.php
+++ bvs-themes/bvs-classic/single.php
@@ -8,17 +8,18 @@ get_header();
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div class="middle">
-    <?php the_date('','<h2>','</h2>'); ?>
-
+<div class="middle wrapper">
+	<div class="post single">
+	<div class="breadCrumb"><a href="<?php bloginfo('home'); ?>">Home</a> / <span class="active"><?php the_title(); ?></span></div>
     <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-         <h3 class="storytitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-
+         <h3 class="storytitle"><?php the_title(); ?></h3>
         <div class="storycontent">
-            <?php the_content(__('(more...)')); ?>
+            <?php the_content(__('(more...)')); ?>            
         </div>
 		<div class="childPages">
-			<ul>
+
+
+			<ul>                
 			  <?php
 				 global $id;
 				 $post_type = get_post_type( $id );
@@ -26,20 +27,24 @@ get_header();
 			  ?>
 			</ul>
 		</div>
-        <div class="feedback">
+        
+        <!--div class="feedback">
             <?php wp_link_pages(); ?>
             <?php comments_popup_link(__('Comments (0)'), __('Comments (1)'), __('Comments (%)')); ?>
-        </div>
-
+        </div-->
     </div>
-
-    <?php comments_template(); // Get wp-comments.php template ?>
-
+	</div>
     <?php endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
     <?php endif; ?>
-
+	<div class="thirdColumn">
+		<?php if ( is_active_sidebar( 'vhl_column_3_' . $current_language ) ) : ?>
+            <?php dynamic_sidebar(  'vhl_column_3_' . $current_language ); ?>
+		<?php endif; ?>
+		<div class="spacer"> </div>
+	</div><!--/thirdColumn-->
     <?php posts_nav_link(' &#8212; ', __('&laquo; Newer Posts'), __('Older Posts &raquo;')); ?>
 
+	<div class="spacer"> </div>
 </div>
 <?php get_footer(); ?>
