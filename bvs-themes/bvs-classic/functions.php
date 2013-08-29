cdcf2ffc5e54c4795c69c798c47d0e73313924ce
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-08-29 14:55:47 -0300

    Make strings i18n

--- bvs-themes/bvs-classic/functions.php
+++ bvs-themes/bvs-classic/functions.php
@@ -9,42 +9,38 @@ automatic_feed_links();
 
 if ( function_exists('register_sidebar') )
     register_sidebar(
-        array('name'=>'Coluna 1 ' .  $current_language,
+        array('name'=>__('Column 1', 'vhl'),
             'id' => 'vhl_column_1_' . $current_language,
-            'description' => __('Rede Social da BVS', 'example'),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<h3 class="widgettitle"><span>',
             'after_title' => '</span></h3>',
         ));
     register_sidebar(
-        array('name'=>'Coluna 2 ' . $current_language, 
+        array('name'=>_('Column 2', 'vhl'),
             'id' => 'vhl_column_2_' . $current_language,
-            'description' => __('Rede de Conteúdos da BVS', 'example'),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<h3 class="widgettitle"><span>',
             'after_title' => '</span></h3>',
         ));
     register_sidebar(
-        array('name'=>'Coluna 3 ' . $current_language, 
+        array('name'=>_('Column 3', 'vhl'),
             'id' => 'vhl_column_3_' . $current_language,
-            'description' => __('Rede de Notícias', 'example'),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<h3 class="widgettitle"><span>',
             'after_title' => '</span></h3>',
         ));
 	   register_sidebar(
-        array('name'=>'Footer ' . $current_language, 
+        array('name'=>_('Footer', 'vhl'),
             'id' => 'vhl_footer_' . $current_language,
-            'description' => __('Rodapé', 'example'),
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<h3 class="widgettitle"><span>',
             'after_title' => '</span></h3>',
         ));
-     
+
      if (function_exists('add_theme_support')) {
         add_theme_support('post-thumbnails');
 		set_post_thumbnail_size(200, 70, true);
