2ee30faa2e48b740157b9b3e3aaf98b7a7bcc4b4
Wilson Moura <mourawil@paho.com>
2013-09-06 16:59:12 -0300

    VHL Network layout normalized

--- bvs-themes/bvs-2012/js/network.js
+++ bvs-themes/bvs-2012/js/network.js
@@ -1,11 +1,17 @@
 // Show Hide VHL Network
 
-var vector = [ 'cl_bvs', 'sub_bvs', 'cl_cvsp', 'cl_scielo', 'sub_scielo' ];
+var vector = [ 'cl_bvs', 'sub_bvs', 'subdev_bvs', 'cl_cvsp', 'cl_scielo', 'sub_scielo', 'subdev_scielo' ];
+var imgpath   = "./wp-content/plugins/bvs-site/bvs-themes/bvs-2012/bireme_archives/default/";
+var image  = null;
 
 $(document).ready(function(){
+	$(".closed").next().hide();
+	$(".closed").css({ "background": "url("+ plus +") no-repeat scroll 0 5px", "padding-left": "12px" });
 	$(".closed", this).click(function(){
+		$(this).next().css("display") == "block" ? image = imgpath + "icon_plus.gif" : image = imgpath + "icon_minus.gif";
+		$(this).css({ "background": "url("+ image +") no-repeat scroll 0 5px", "padding-left": "12px" });
 		if(jQuery.inArray($(this).next().attr("id"), vector) != -1){
-			$(this).next().toggle('slow');
+			$(this).next().toggle("slow");
 			return false;
 		}
 	});
