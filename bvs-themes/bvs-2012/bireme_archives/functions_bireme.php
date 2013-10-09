4ab579371775a5b047fb6c952d8dddf0ee78ab9e
Renato Murasaki <renato.murasaki@gmail.com>
2013-10-09 18:49:26 -0300

    Changing  bir_show_custom_field function to receive html elements to format label and custom field value. Only display label and custom field value if the field is different of null.

--- bvs-themes/bvs-2012/bireme_archives/functions_bireme.php
+++ bvs-themes/bvs-2012/bireme_archives/functions_bireme.php
@@ -5,14 +5,6 @@
  *
  */
  /* Load up our theme options page and related code. */
-$current_language = strtolower(get_bloginfo('language'));
-
-if ($current_language != ''){
-   $current_language = '_' . $current_language;
-}
-
-load_plugin_textdomain( 'vhl', false,  BVS_PLUGIN_DIR . '/languages' );
-
 if ( is_admin() ) require_once( TEMPLATEPATH . '/bireme_archives/admin_settings.php' );
 
 $settings = get_option( "wp_bvs_theme_settings" );
@@ -23,8 +15,8 @@ $footer_sidebar = $layout['footer-sidebar'];
 
 // sidebars do template
 register_sidebar( array(
-    'name' => __('Header','vhl'),
-    'id' => 'header' . $current_language,
+    'name' => 'Header',
+    'id' => 'header',
     'description' => '',
     'before_widget' => '<div id="%1$s" class="widget %2$s">',
     'after_widget' => '</div>',
@@ -35,8 +27,8 @@ register_sidebar( array(
 //SideBar Auxiliar Top só aparece se ativado
 if ($top_sidebar == true){
     register_sidebar( array(
-        'name' => __('Top Auxiliary SideBar','vhl'),
-        'id' => 'top_sidebar' . $current_language,
+        'name' => 'SideBar Auxiliar Top',
+        'id' => 'top_sidebar',
         'description' => '',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget' => '</aside>',
@@ -49,8 +41,8 @@ if ($top_sidebar == true){
 for($i=1; $i <= $total_columns; $i++) {
 
     register_sidebar( array(
-        'name' => __('Column', 'vhl') . ' ' . $i,
-        'id' => 'column-' . $i . $current_language,
+        'name' => 'Coluna ' . $i,
+        'id' => 'column-' . $i,
         'description' => '',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget' => '</aside>',
@@ -62,8 +54,8 @@ for($i=1; $i <= $total_columns; $i++) {
 //SideBar Auxiliar Footer só aparece se ativado
 if ($footer_sidebar == true){
     register_sidebar( array(
-        'name' => __('Footer Auxiliary SideBar','vhl'),
-        'id' => 'footer_sidebar' . $current_language,
+        'name' => 'SideBar Auxiliar Footer',
+        'id' => 'footer_sidebar',
         'description' => '',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget' => '</aside>',
@@ -73,8 +65,8 @@ if ($footer_sidebar == true){
 }
 
 register_sidebar( array(
-    'name' => __('Footer','vhl'),
-    'id' => 'footer' . $current_language,
+    'name' => 'Footer',
+    'id' => 'footer',
     'description' => '',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => '</aside>',
@@ -83,8 +75,9 @@ register_sidebar( array(
 ) );
 
 register_sidebar( array(
-    'name' => __('Level 2','vhl'),
-    'id' => 'level2' . $current_language,
+    'name' => 'Level2',
+    'id' => 'level2',
+    'description' => 'Widgets que aparecerão em segundo nível',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => '</aside>',
     'before_title' => '<strong class="widget-title">',
@@ -106,5 +99,38 @@ if(file_exists($custom_include_file)) {
     require_once($custom_include_file);
 }
 
-add_filter('widget_text', 'do_shortcode');
+// Display the value of custom fields 
+function bir_show_custom_field($post_id, $key, $label="", $html4label="", $html4custom_field="", $single=true, $separator=",") {
+/*
+	Samples for $html4label and $html4custom_field. Keep always the strings "label" and "custom_field", because the function will replace them using regular expression.
+
+	$html4label
+		"<li>label</li>"
+		"<dt>label</dt>"
+	$html4custom_field
+		"<li>custom_field</li>"
+		"<dd>custom_field</dd>"
+		"<p>custom_field</p>"
+*/	
+
+	$customField = get_post_meta($post_id, $key, $single);
+	
+	if (!is_array($customField)) {
+		if (trim($customField)!= "") {
+			echo preg_replace("/label/", $label, $html4label);
+			echo preg_replace("/custom_field/", $customField, $html4custom_field);
+		}
+	} else {
+		echo preg_replace("/label/", $label, $html4label);
+		$count = count($customField);
+		$lastValue = end($customField);
+		$text = "";
+		foreach ( $customField as $value) {
+			$text .= $value;	
+			if ($value != $lastValue) $text .= $separator . " ";
+		}	 
+		echo preg_replace("/custom_field/", $text, $html4custom_field);
+	}
+}
+
 ?>
