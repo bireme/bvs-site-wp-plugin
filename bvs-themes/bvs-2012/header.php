9e9e25957fb7da37d5d52ecd322ff9a4a96df639
juliotak <takayama@paho.org>
2014-04-25 16:20:47 -0300

    changing doctype specification to HTML5

--- bvs-themes/bvs-2012/header.php
+++ bvs-themes/bvs-2012/header.php
@@ -8,35 +8,30 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
-
 $mlf_options = get_option('mlf_config');
 $current_language = strtolower(get_bloginfo('language'));
 $site_lang = substr($current_language, 0,2);
-
 if ($current_language != ''){
 	$current_language = '_' . $current_language;
 }
-
 $top = "header";
-
 if(is_plugin_active('multi-language-framework/multi-language-framework.php'))
 	$top .= $current_language;
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-	<!--[if IE 7]>
-	<html class="ie ie7" <?php language_attributes(); ?>>
-	<![endif]-->
-	<!--[if IE 8]>
-	<html class="ie ie8" <?php language_attributes(); ?>>
-	<![endif]-->
-	<!--[if !(IE 7) | !(IE 8)  ]><!-->
-	<html xmlns="http://www.w3.org/1999/xhtml" xml:<?php language_attributes(); ?>>
-	<!--<![endif]-->
+<!DOCTYPE HTML>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
 	
 	<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="viewport" content="width=device-width, user-scalable=no" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -45,20 +40,14 @@ if(is_plugin_active('multi-language-framework/multi-language-framework.php'))
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
-	
 	<noscript>Your browser does not support JavaScript!</noscript>
-	
 	<!-- extract the admin configs -->
 	<?php include "bireme_archives/admin_configs.php"; ?>
-
 	<!-- wp_head -->
 	<?php wp_head(); ?>
-
 	<!-- block extrahead -->
 	<?= stripslashes( $header['extrahead'] ) ?>
-
 	<!-- block extra files -->
-
 	</head>
 
 	<body <?php body_class(); ?>>
@@ -79,7 +68,7 @@ if(is_plugin_active('multi-language-framework/multi-language-framework.php'))
 				<?php } ?>
 				<?php wp_nav_menu( array( 'fallback_cb' => 'false' ) ); ?>
 			</div>
-	        <div class="top top<?php echo ($current_language);?>">
+	        <div class="top top_<?php echo ($current_language);?>">
 	            <div id="parent">
 	            	<a href="<?php echo $linkLogo;?>" title="<?php echo __('Virtual Health Library','vhl');?>"> 
 		                <img src="<?php echo $logo;?>" alt="<?php echo __('VHL Logo','vhl');?>"/>
