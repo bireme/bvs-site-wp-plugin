358e8cc4779ee93a7b4b62ceb438794aeba43b74
Moacir Moda <moa.moda@gmail.com>
2015-08-27 11:09:07 -0300

    adicionando quantidade de resultados

--- bvs-widgets/vhl_clusters_widget.php
+++ bvs-widgets/vhl_clusters_widget.php
@@ -26,7 +26,15 @@ class VHL_Clusters_Widget extends WP_Widget {
         if(!empty($instance['clusters']) and !empty($instance['cluster'])) {
 
             print "<ul>";
+            $count = 0;
             foreach($instance['clusters'][$instance['cluster']] as $cluster_content) {
+
+                if($count >= (int)$instance['results']) {
+                    break;
+                } else {
+                    $count++;
+                }
+
 
                 $lang_key = "REFINE_" . $instance['cluster'];
                 $array_lang = $instance['language_content'];
@@ -56,6 +64,7 @@ class VHL_Clusters_Widget extends WP_Widget {
         $instance['cluster'] = strip_tags($new_instance['cluster']);
         $instance['title'] = strip_tags($new_instance['title']);
         $instance['url'] = strip_tags($new_instance['url']);
+        $instance['results'] = strip_tags($new_instance['results']);
 
         if(!empty($instance['url']) and strpos($instance['url'], '?') === false) {
             $instance['url'] = $instance['url'] . "?";
@@ -89,6 +98,7 @@ class VHL_Clusters_Widget extends WP_Widget {
         
         $title = esc_attr($instance['title']);
         $url = esc_attr($instance['url']);
+        $results = esc_attr($instance['results']);
 
         // var_dump($instance['language_content']);
 
@@ -122,6 +132,13 @@ class VHL_Clusters_Widget extends WP_Widget {
                         </select>
                     </label>
                 </p>                
+
+                <p>
+                    <label for="<?php echo $this->get_field_id('results'); ?>">
+                        <?php _e('Number of Results:', 'vhl'); ?> 
+                        <input class="widefat" id="<?php echo $this->get_field_id('results'); ?>" name="<?php echo $this->get_field_name('results'); ?>" type="number" value="<?php echo $results; ?>" />
+                    </label>
+                </p>
             <?php endif; ?>
 
         <?php 
